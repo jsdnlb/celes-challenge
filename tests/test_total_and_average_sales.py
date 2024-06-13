8062a10465f06e725fe44e637c1b61b321fe50ab
Daniel Buitrago <danielb.alzate1@gmail.com>
2024-06-13 02:13:13 -0500

    Add tests for test_total_and_average_sales and utils

---
+++ tests/test_total_and_average_sales.py
@@ -0,0 +1,127 @@
+import unittest
+from fastapi.testclient import TestClient
+from unittest.mock import patch
+from main import app
+from tests.utils.utils_test import (
+    setup_test_environment,
+    teardown_test_environment,
+    sort_results,
+)
+
+client = TestClient(app)
+
+
+class TestTotalAndAverageSales(unittest.TestCase):
+
+    def setUp(self):
+        self.data_path = "test_data/"
+        self.ROUTE_ENDPOINT = "/api/sales/total_and_average_sales/"
+        setup_test_environment(self.data_path)
+
+    def tearDown(self):
+        teardown_test_environment(self.data_path)
+
+    def get_valid_token(self):
+        with patch("firebase_admin.auth"):
+            response = client.post(
+                "/api/users/token",
+                data={"username": "test@example.com", "password": "password"},
+            )
+            assert response.status_code == 200
+            return response.json()["access_token"]
+
+    """ 
+    The following tests validate that the endpoint brings the average and
+    total amount for KeyStore, KeyProdiuct and KeyEmployee.
+    """
+
+    def test_total_and_average_sales_keystore(self):
+        token = self.get_valid_token()
+        response = client.get(
+            self.ROUTE_ENDPOINT,
+            headers={"Authorization": f"Bearer {token}"},
+            params={"query_key": "KeyStore", "data_path": self.data_path},
+        )
+        expected_response = [
+            {"KeyStore": 1001, "TotalAmount": 350.0, "AvgAmount": 175.0},
+            {"KeyStore": 1002, "TotalAmount": 1300.0, "AvgAmount": 650.0},
+        ]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            sort_results(response.json(), "KeyStore"),
+            sort_results(expected_response, "KeyStore"),
+        )
+
+    def test_total_and_average_sales_keyproduct(self):
+        token = self.get_valid_token()
+        response = client.get(
+            self.ROUTE_ENDPOINT,
+            headers={"Authorization": f"Bearer {token}"},
+            params={"query_key": "KeyProduct", "data_path": self.data_path},
+        )
+        expected_response = [
+            {
+                "KeyProduct": 101,
+                "TotalAmount": 1450.0,
+                "AvgAmount": 483.3333333333333,
+            },
+            {"KeyProduct": 102, "TotalAmount": 200.0, "AvgAmount": 200.0},
+        ]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            sort_results(response.json(), "KeyProduct"),
+            sort_results(expected_response, "KeyProduct"),
+        )
+
+    def test_total_and_average_sales_keyemployee(self):
+        token = self.get_valid_token()
+        response = client.get(
+            self.ROUTE_ENDPOINT,
+            headers={"Authorization": f"Bearer {token}"},
+            params={"query_key": "KeyEmployee", "data_path": self.data_path},
+        )
+        expected_response = [
+            {
+                "KeyEmployee": 1,
+                "TotalAmount": 350.0,
+                "AvgAmount": 175.0,
+            },
+            {"KeyEmployee": 2, "TotalAmount": 300.0, "AvgAmount": 300.0},
+            {"KeyEmployee": 3, "TotalAmount": 1000.0, "AvgAmount": 1000.0},
+        ]
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            sort_results(response.json(), "KeyEmployee"),
+            sort_results(expected_response, "KeyEmployee"),
+        )
+
+    def test_total_and_average_sales_invalid_key(self):
+        """
+        This test validates the exception that we must enter a valid query_key.
+        """
+        token = self.get_valid_token()
+        response = client.get(
+            self.ROUTE_ENDPOINT,
+            headers={"Authorization": f"Bearer {token}"},
+            params={"query_key": "InvalidKey", "data_path": self.data_path},
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {"detail": "Enter a valid query key"})
+
+    def test_total_and_average_sales_without_token(self):
+        """
+        This test validates that we are logged in to access the endpoint.
+        """
+        response = client.get(
+            self.ROUTE_ENDPOINT,
+            headers={"Authorization": f"Bearer fake-token"},
+            params={"query_key": "KeyStore", "data_path": self.data_path},
+        )
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(
+            response.json(), {"detail": "Invalid authentication credentials"}
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/utils/utils_test.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+from pyspark.sql import SparkSession
+
+
+def create_test_data(data_path):
+    """
+    Creates the necessary files for editing and testing
+    """
+    spark = SparkSession.builder.appName("Test Data").getOrCreate()
+    data = [
+        {
+            "KeyDate": "2024-01-01",
+            "KeyEmployee": 1,
+            "KeyProduct": 101,
+            "KeyStore": 1001,
+            "Amount": 150.0,
+        },
+        {
+            "KeyDate": "2024-01-02",
+            "KeyEmployee": 1,
+            "KeyProduct": 102,
+            "KeyStore": 1001,
+            "Amount": 200.0,
+        },
+        {
+            "KeyDate": "2024-01-03",
+            "KeyEmployee": 2,
+            "KeyProduct": 101,
+            "KeyStore": 1002,
+            "Amount": 300.0,
+        },
+        {
+            "KeyDate": "2024-01-04",
+            "KeyEmployee": 3,
+            "KeyProduct": 101,
+            "KeyStore": 1002,
+            "Amount": 1000.0,
+        },
+    ]
+    df = spark.createDataFrame(data)
+    df.write.parquet(data_path, mode="overwrite")
+    return df
+
+
+def setup_test_environment(data_path):
+    """
+    Creates the folder if it does not exist where the files will be stored
+    """
+    os.makedirs(data_path, exist_ok=True)
+    create_test_data(data_path)
+
+
+def teardown_test_environment(data_path):
+    """
+    Delete files after testing is completed
+    """
+    if os.path.exists(data_path):
+        shutil.rmtree(data_path)
+
+
+def sort_results(results, key):
+    """
+    Order the answers as sometimes some tests failed due to the order of the answers.
+    """
+    return sorted(results, key=lambda x: x[key])
