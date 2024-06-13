c8d64036eb3c3600d2eb9e45433e9fb95bed45cf
Daniel Buitrago <danielb.alzate1@gmail.com>
2024-06-12 20:01:29 -0500

    Add test for endpoint sales_by_time_period

---
+++ tests/test_sales_by_time_period.py
@@ -0,0 +1,181 @@
+import unittest
+from unittest.mock import patch
+from fastapi.testclient import TestClient
+from main import app
+
+
+class TestSalesByTimePeriod(unittest.TestCase):
+    def setUp(self):
+        self.client = TestClient(app)
+        self.valid_token = self.get_valid_token()
+        self.ROUTE_ENDPOINT = "/api/sales/sales_by_time_period/"
+
+    def get_valid_token(self):
+        with patch("firebase_admin.auth"):
+            # Simulate POST request to obtain an authorization token
+            response = self.client.post(
+                "/api/users/token",
+                data={"username": "test@example.com", "password": "password"},
+            )
+
+            assert response.status_code == 200
+
+            # Extract the token from the body of the response and return it
+            return response.json()["access_token"]
+
+    """
+    The following tests validate the correct operation of the endpoint
+    """
+
+    def test_valid_request_with_keyemployee(self):
+        """
+        Test to validate the correct operation with queryKey KeyEmployee
+        """
+        response = self.client.get(
+            self.ROUTE_ENDPOINT,
+            params={
+                "start_date": "2023-01-01",
+                "end_date": "2023-01-02",
+                "queryKey": "KeyEmployee",
+            },
+            headers={"Authorization": f"Bearer {self.valid_token}"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+
+    def test_valid_request_with_keystore(self):
+        """
+        Test to validate the correct operation with queryKey KeyStore
+        """
+        response = self.client.get(
+            self.ROUTE_ENDPOINT,
+            params={
+                "start_date": "2023-01-01",
+                "end_date": "2023-01-02",
+                "queryKey": "KeyStore",
+            },
+            headers={"Authorization": f"Bearer {self.valid_token}"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+
+    def test_valid_request_with_keyproduct(self):
+        """
+        Test to validate the correct operation with queryKey KeyProduct
+        """
+        response = self.client.get(
+            self.ROUTE_ENDPOINT,
+            params={
+                "start_date": "2023-01-01",
+                "end_date": "2023-01-02",
+                "queryKey": "KeyProduct",
+            },
+            headers={"Authorization": f"Bearer {self.valid_token}"},
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json(), list)
+
+    def test_invalid_date_range(self):
+        """
+        Test to validate that start date is greater than end date
+        """
+        response = self.client.get(
+            self.ROUTE_ENDPOINT,
+            params={
+                "start_date": "2023-01-02",
+                "end_date": "2023-01-01",
+                "queryKey": "KeyEmployee",
+            },
+            headers={"Authorization": f"Bearer {self.valid_token}"},
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.json()["detail"], "Start date must be greater than end date"
+        )
+
+    """ 
+    The following tests validate that the data is as expected
+    """
+
+    def test_invalid_query_key(self):
+        """
+        Test to validate that input fails when entering an invalid queryKey
+        """
+        response = self.client.get(
+            self.ROUTE_ENDPOINT,
+            params={
+                "start_date": "2023-01-01",
+                "end_date": "2023-01-02",
+                "queryKey": "InvalidKey",
+            },
+            headers={"Authorization": f"Bearer {self.valid_token}"},
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json()["detail"], "Enter a valid queryKey")
+
+    def test_to_validate_input_year(self):
+        """
+        Test to validate the year's input data
+        """
+        response = self.client.get(
+            self.ROUTE_ENDPOINT,
+            params={
+                "start_date": "20-01-01",
+                "end_date": "2023-01-02",
+                "queryKey": "KeyProduct",
+            },
+            headers={"Authorization": f"Bearer {self.valid_token}"},
+        )
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            response.json()["detail"][0]["msg"],
+            "Input should be a valid date or datetime, input is too short",
+        )
+
+    def test_to_validate_input_month(self):
+        """
+        Test to validate the input data of the month greater than 12 in end_date
+        """
+        response = self.client.get(
+            self.ROUTE_ENDPOINT,
+            params={
+                "start_date": "2023-01-01",
+                "end_date": "2023-13-02",
+                "queryKey": "KeyProduct",
+            },
+            headers={"Authorization": f"Bearer {self.valid_token}"},
+        )
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            response.json()["detail"][0]["msg"],
+            "Input should be a valid date or datetime, month value is outside expected range of 1-12",
+        )
+
+    def test_to_validate_input_day(self):
+        """
+        Test to validate the input data of the day greater than 31 in end_date
+        """
+        response = self.client.get(
+            self.ROUTE_ENDPOINT,
+            params={
+                "start_date": "2023-01-32",
+                "end_date": "2023-13-02",
+                "queryKey": "KeyProduct",
+            },
+            headers={"Authorization": f"Bearer {self.valid_token}"},
+        )
+
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(
+            response.json()["detail"][0]["msg"],
+            "Input should be a valid date or datetime, day value is outside expected range",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
